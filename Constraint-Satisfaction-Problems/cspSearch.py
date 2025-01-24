0eeb788a668aae9cf03f1a43681b9e236117d7b7
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2025-01-24 10:17:24 -0300

    feat: add csps to saerch problem algorithm

---
+++ Constraint-Satisfaction-Problems/cspSearch.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append('../SearchProblem')
+from searchProblem import Arc, Search_problem
+
+
+class Search_from_CSP(Search_problem):
+
+    def __init__(self, csp, variable_order=None):
+
+        self.csp = csp
+
+        if(variable_order):
+            assert set(variable_order) == set(csp.variables)
+            assert len(variable_order) == len(csp.variables)
+            self.variables = variable_order
+        else:
+            self.variables = list(csp.variables)
+
+    def is_goal(self, node):
+
+        return len(node) == len(self.csp.variables)
+
+    def start_node(self):
+
+        return {}
+
+    def neighbors(self, node):
+
+        var = self.variables[len(node.domain)]
+        res = []
+
+        for val in var.domain:
+            new_env = node | {val: val}
+
+            if(self.csp.consistent(new_end)):
+                res.append(Arc(node, new_env))
+
+        return res
+
+def solver_from_searcher(csp):
+
+    path = Searcher(Search_from_CSP(csp)).search() #generic searcher from Search_Problem
+
+    if(path is not None):
+        return path.end()
+    else:
+        return None

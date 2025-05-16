330b28e3c474bd55f2a12a42efc2bee92e95dd4f
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2025-05-16 11:53:23 -0300

    feat: add parameter to save the best model to a pkl file

--- ai-frameworks/models/training.py
+++ ai-frameworks/models/training.py
@@ -1,9 +1,11 @@
 from preprocessing import build_pipeline
 from sklearn.model_selection import cross_val_score, GridSearchCV
 import numpy as np
+import joblib
 
 
-def train_model(data, target, model=None, param_grid=None, num_imputer="mean", cat_imputer="most_frequent", cv=5):
+def train_model(data, target, model=None, param_grid=None, num_imputer="mean",
+                cat_imputer="most_frequent", cv=5, filename=None):
     """
     Train a model using a specified target feature.
     
@@ -15,6 +17,7 @@ def train_model(data, target, model=None, param_grid=None, num_imputer="mean", c
     - num_imputer_strategy: Strategy for numeric imputers ('mean', 'median', 'most_frequent', 'constant')
     - cat_imputer_strategy: Strategy for categorical imputers ('most_frequent', 'constant')
     - cv: Int, number of cross-validation folds
+    - filename: Get the file name to save the best model, format: pkl
 
     Returns:
     - dict: A dictionary containing:
@@ -48,7 +51,6 @@ def train_model(data, target, model=None, param_grid=None, num_imputer="mean", c
         best_model = grid_search.best_estimator_
         best_params = grid_search.best_params_
         scores = np.sqrt(-grid_search.best_score_)
-
         print("best parameters : ", best_params)
         print("best score : ", scores)
     else:
@@ -58,10 +60,14 @@ def train_model(data, target, model=None, param_grid=None, num_imputer="mean", c
         scores = np.sqrt(-cv_scores)
         print("score : ", scores)
 
+    if(filename):
+        joblib.dump(best_model, filename)
+        
     predictions = best_model.predict(X_test)
-
+        
     return {
+        "best_model": best_model,
         "predictions": predictions,
+        "best_params": best_params,
         "scores": scores,
-        "best_params": best_params
         }

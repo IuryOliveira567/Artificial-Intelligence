c94fd2b4fd1f8f60077f10807b5ac9bd97c390b0
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2026-04-13 11:11:44 -0300

    fix: corretion to the load_data function

--- neural-networks/data/mnist_loader.py
+++ neural-networks/data/mnist_loader.py
@@ -1,4 +1,11 @@
+import os
 from tensorflow.keras.datasets import mnist
 
 def load_data():
-    return mnist.load_data()
+    path = "raw/mnist.npz"
+    
+    if os.path.exists(path):
+        return mnist.load_data(path)
+    else:
+        data = mnist.load_data()
+        return data

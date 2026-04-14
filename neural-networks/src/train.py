0c327dfbd28eede8de2ae70279759914899f0990
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2026-04-14 14:36:53 -0300

    feat: add function to compile and fit the classification model

--- neural-networks/src/train.py
+++ neural-networks/src/train.py
@@ -1,4 +1,24 @@
+from src.models.sklearn_model import build_model as build_sklearn
+from src.models.keras_model import build_model as build_keras
+
+
 def train_sklearn(X, y):
     model = build_sklearn()
+
+    X = X.reshape(len(X), -1)
     model.fit(X.reshape(len(X), -1), y)
+
+    return model
+
+def train_keras(X, y):
+    model = build_keras()
+    
+    model.compile(
+        optimizer='sgd',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    
+    model.fit(X, y, epochs=10, validation_split=0.1)
+    
     return model

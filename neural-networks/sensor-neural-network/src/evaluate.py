7eef4f98c5f5d2d1b290ddc8d382f73f74e76e56
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2026-05-27 10:48:23 -0300

    feat: added evaluation function for trained model

---
+++ neural-networks/sensor-neural-network/src/evaluate.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+
+
+def plot_history(history):
+
+    plt.figure(figsize=(10, 5))
+
+    plt.plot(history.history["accuracy"])
+    plt.plot(history.history["val_accuracy"])
+
+    plt.title("Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+
+    plt.legend([
+        "Train",
+        "Validation"
+    ])
+
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(figsize=(10, 5))
+
+    plt.plot(history.history["loss"])
+    plt.plot(history.history["val_loss"])
+
+    plt.title("Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+
+    plt.legend([
+        "Train",
+        "Validation"
+    ])
+
+    plt.grid(True)
+    plt.show()
